98842fcc9071b043fa2bfa5bbf67dcebfc4623ca
Mario Moreno <m4mc3r@gmail.com>
2025-04-04 18:42:35 -0300

    Add Nostalgia.Core entity

---
+++ Nostalgia.Core/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace Nostalgia.Core;
+
+public class Item
+{
+    public string Name { get; set; } = string.Empty;
+    public string Path { get; set; } = string.Empty;
+    public string Hash { get; set; } = string.Empty;
+}
