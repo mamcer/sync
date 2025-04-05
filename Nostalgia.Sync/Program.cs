7f14054737bf399308306c05bd46ad651614c177
Mario Moreno <m4mc3r@gmail.com>
2025-04-05 13:26:42 -0300

    Add initial directory scanner

--- Nostalgia.Sync/Program.cs
+++ Nostalgia.Sync/Program.cs
@@ -1,9 +1,42 @@
-﻿namespace Nostalgia.Sync;
+﻿using System.Diagnostics;
+
+namespace Nostalgia.Sync;
 
 static class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, Nostalgia Sync");
+        var timer = new Stopwatch();
+        timer.Start();
+        Console.WriteLine($"process started {timer.Elapsed}");
+
+        // read
+
+        // Specify the directory to read
+        string directoryPath = "/home/mario/Desktop/nostalgia";
+
+        var paths = new List<string>(){directoryPath};
+
+        for(int i=0; i<paths.Count; i++) 
+        {
+            var files = Directory.GetFiles(paths[i]);
+            foreach (var file in files)
+            {
+                Console.WriteLine($"\tfound file: {file}");
+            }
+
+            var directories = Directory.GetDirectories(paths[i]);
+            foreach (var directory in directories)
+            {
+                paths.Add(directory);
+            }
+        }
+
+        // hash 
+
+        // persist
+
+        Console.WriteLine($"process finished {timer.Elapsed}");
+        timer.Stop();
     }
 }
