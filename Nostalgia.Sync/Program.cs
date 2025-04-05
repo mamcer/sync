30e1bbf60a1c007e76cdd51c4d5bc642bd865825
Mario Moreno <m4mc3r@gmail.com>
2025-04-05 13:28:05 -0300

    Add file count and directory count

--- Nostalgia.Sync/Program.cs
+++ Nostalgia.Sync/Program.cs
@@ -11,24 +11,25 @@ static class Program
         Console.WriteLine($"process started {timer.Elapsed}");
 
         // read
-
-        // Specify the directory to read
         string directoryPath = "/home/mario/Desktop/nostalgia";
 
         var paths = new List<string>(){directoryPath};
-
+        int fileCount = 0;
+        int directoryCount = 0;
         for(int i=0; i<paths.Count; i++) 
         {
             var files = Directory.GetFiles(paths[i]);
             foreach (var file in files)
             {
                 Console.WriteLine($"\tfound file: {file}");
+                fileCount++;
             }
 
             var directories = Directory.GetDirectories(paths[i]);
             foreach (var directory in directories)
             {
                 paths.Add(directory);
+                directoryCount++;
             }
         }
 
@@ -36,6 +37,7 @@ static class Program
 
         // persist
 
+        Console.WriteLine($"directory count: {directoryCount}\nfiles: {fileCount}");
         Console.WriteLine($"process finished {timer.Elapsed}");
         timer.Stop();
     }
